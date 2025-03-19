189d0857e8b2218fe9e6efff8a87170037a7194c
Ian Isaacson <ian_isaacson@yahoo.com>
2025-03-19 14:52:00 -0700

    bubbleSort passed tests

--- bubble.js
+++ bubble.js
@@ -1,3 +1,23 @@
-function bubbleSort() {}
+function bubbleSort(arr) {
+  console.log("🚀 ~ bubbleSort ~ starting arr:", arr);
+
+  const length = arr.length;
+
+  if (length <= 1) {
+    return arr;
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 1; j < arr.length - i; j++) {
+      if (arr[j - 1] > arr[j]) {
+        let temp = arr[j - 1];
+        arr[j - 1] = arr[j];
+        arr[j] = temp;
+      }
+    }
+    console.log("🚀 ~ bubbleSort ~ arr:", arr);
+  }
+  return arr;
+}
 
 module.exports = bubbleSort;
