f15edcd54bf954bffdc7621eb69f703a36f379f9
Ian Isaacson <ian_isaacson@yahoo.com>
2025-03-21 11:50:53 -0700

    mergeSort working!

--- merge.js
+++ merge.js
@@ -1,44 +1,123 @@
-// first attempt
+// Also copies rest of remaining array after one is exhausted in one chunk instead of iterating
 function merge(arr1, arr2) {
-  console.log("🚀 ~ merge ~ arr1:", arr1);
-  console.log("🚀 ~ merge ~ arr2:", arr2);
   const length1 = arr1.length;
   const length2 = arr2.length;
-  let sortedArr = [];
+  let sortedArr = Array.from({ length: length1 + length2 }); // Preallocates size of sorted array with combined size of input arrays
 
-  let i = 0;
-  let j = 0;
+  let i = 0; // Index for array 1
+  let j = 0; // Index for array 2
+  let k = 0; // Index for sorted array to avoid using push
 
   while (i < length1 && j < length2) {
-    if (arr1[i] < arr2[j]) {
-      sortedArr.push(arr1[i]);
-      i++;
-    } else {
-      sortedArr.push(arr2[j]);
-      j++;
-    }
-    console.log("🚀 ~ merge ~ sortedArr:", sortedArr);
+    sortedArr[k++] = arr1[i] < arr2[j] ? arr1[i++] : arr2[j++];
+    // Adds the smaller number to the sorted array at index [k],
+    // increments the index for the array it came from appropriately,
+    // and then increments the index for the sorted array with [k++].
   }
 
-  while (i < length1) {
-    sortedArr.push(arr1[i]);
-    i++;
-  }
+  if (i < length1) sortedArr.splice(k, length1 - i, ...arr1.slice(i));
+  //
 
-  while (j < length2) {
-    sortedArr.push(arr2[j]);
-    j++;
-  }
-  console.log("🚀 ~ added remaining ~ sortedArr:", sortedArr);
+  if (j < length2) sortedArr.splice(k, length2 - j, ...arr2.slice(j));
+
+  // console.log("🚀 ~ added remaining ~ sortedArr:", sortedArr);
 
   return sortedArr;
 }
 
-function mergeSort(arr) {}
+function mergeSort(arr) {
+  // 1. Break up the array into halves until arrays are 0 or 1 elements in length (inherently sorted) (recursive)
+  // 2. Merge with other sorted arrays (recursive) ( use merge() )
+  // 3. Return the merged (and sorted!) array
+
+  //   mergeSort(arrSegment);
+  //   merge();
+
+  const length = arr.length;
+
+  // Arrays with 1 or 0 elements are already sorted
+  if (length <= 1) return arr;
+
+  // Find half length of array, accounting for odd lengths
+  const half = Math.floor(length / 2);
+
+  // Split array
+  const split1 = mergeSort(arr.slice(0, half));
+  const split2 = mergeSort(arr.slice(half));
+
+  // Compare, sort, return
+  return merge(split1, split2);
+}
+
+// Optimized with prealloated space + manually tracks sorted array index
+// function merge(arr1, arr2) {
+//   const length1 = arr1.length;
+//   const length2 = arr2.length;
+//   let sortedArr = Array.from({ length: length1 + length2 }); // Preallocates size of sorted array with combined size of input arrays
+
+//   let i = 0; // Index for array 1
+//   let j = 0; // Index for array 2
+//   let k = 0; // Index for sorted array to avoid using push
+
+//   while (i < length1 && j < length2) {
+//     sortedArr[k++] = arr1[i] < arr2[j] ? arr1[i++] : arr2[j++];
+//     // Adds the smaller number to the sorted array at index [k],
+//     // increments the index for the array it came from appropriately,
+//     // and then increments the index for the sorted array with [k++].
+//   }
+
+//   while (i < length1) sortedArr[k++] = arr1[i++];
+
+//   while (j < length2) sortedArr[k++] = arr2[j++];
+
+//   // console.log("🚀 ~ added remaining ~ sortedArr:", sortedArr);
+
+//   return sortedArr;
+// }
+
+// function mergeSort(arr) {}
+
+// first (actual) merge() attempt
+// function merge(arr1, arr2) {
+//     console.log("🚀 ~ merge ~ arr1:", arr1);
+//   console.log("🚀 ~ merge ~ arr2:", arr2);
+//   const length1 = arr1.length;
+//   const length2 = arr2.length;
+//   let sortedArr = [];
+
+//   let i = 0;
+//   let j = 0;
+
+//   while (i < length1 && j < length2) {
+//     if (arr1[i] < arr2[j]) {
+//       sortedArr.push(arr1[i]);
+//       i++;
+//     } else {
+//       sortedArr.push(arr2[j]);
+//       j++;
+//     }
+//     console.log("🚀 ~ merge ~ sortedArr:", sortedArr);
+//   }
+
+//   while (i < length1) {
+//     sortedArr.push(arr1[i]);
+//     i++;
+//   }
+
+//   while (j < length2) {
+//     sortedArr.push(arr2[j]);
+//     j++;
+//   }
+//   console.log("🚀 ~ added remaining ~ sortedArr:", sortedArr);
+
+//   return sortedArr;
+// }
+
+// function mergeSort(arr) {}
 
 module.exports = { merge, mergeSort };
 
-// // first attempt
+// // barked up the wrong tree
 // function merge(arr1, arr2) {
 //   const origLength1 = arr1.length;
 //   const origLength2 = arr2.length;
