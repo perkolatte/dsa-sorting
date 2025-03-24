63181f40812afb36449152d3bd6e10ab879f6c57
Ian Isaacson <ian_isaacson@yahoo.com>
2025-03-23 17:41:54 -0700

    selection sort works!

--- selection.js
+++ selection.js
@@ -1,3 +1,20 @@
-function selectionSort() {}
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let smallest = arr[i];
+    let indexOfSmallest = i;
+
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < smallest) {
+        smallest = arr[j];
+        indexOfSmallest = j;
+      }
+    }
+
+    if (indexOfSmallest !== i) {
+      [arr[i], arr[indexOfSmallest]] = [arr[indexOfSmallest], arr[i]];
+    }
+  }
+  return arr;
+}
 
 module.exports = selectionSort;
