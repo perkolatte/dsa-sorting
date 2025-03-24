9654953f3361c80314351ea6cea7ba94a00c3868
Ian Isaacson <ian_isaacson@yahoo.com>
2025-03-23 17:47:46 -0700

    added selection sort comments

--- selection.js
+++ selection.js
@@ -1,8 +1,12 @@
+// First attempt
 function selectionSort(arr) {
+  // Outer loop to track the sorted portion
   for (let i = 0; i < arr.length; i++) {
+    // Reset smallest value and its location
     let smallest = arr[i];
     let indexOfSmallest = i;
 
+    // Inner loop to find the smallest value in the unsorted portion
     for (let j = i + 1; j < arr.length; j++) {
       if (arr[j] < smallest) {
         smallest = arr[j];
@@ -10,10 +14,12 @@ function selectionSort(arr) {
       }
     }
 
+    // Conditional swap if a new smallest value was found
     if (indexOfSmallest !== i) {
       [arr[i], arr[indexOfSmallest]] = [arr[indexOfSmallest], arr[i]];
     }
   }
+  // Return the sorted array.
   return arr;
 }
 
